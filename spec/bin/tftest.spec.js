ee97a9086f323c68e56ac686573272a468e461f0
Renato Castro <renato@yld.io>
2018-08-16 10:45:15 +0100

    Cli tests

--- spec/bin/tftest.spec.js
+++ spec/bin/tftest.spec.js
@@ -1,13 +1,43 @@
 const { show, cli, program } = require('../../bin/tftest');
 
 describe('tftest', () => {
-  describe('show', () => {
-    it('should return false', () => {
-      expect(show('dummy')).toEqual('false');
+  describe('program', () => {
+    it('program test command with arguments', () => {
+      program.parse(['test', '-t', 'bla', '-p', 'ble']);
+      expect(program.commands[0].options[0].required).not.toBeFalsy();
+      expect(program.commands[0].options[0].flags).toEqual('-t, --terraformFolder <terraformFolder>');
+      expect(program.commands[0].options[0].short).toEqual('-t');
+      expect(program.commands[0].options[0].long).toEqual('--terraformFolder');
+      expect(program.commands[0].options[1].required).not.toBeFalsy();
+      expect(program.commands[0].options[1].flags).toEqual('-p, --terraformPlan <terraformPlan>');
+      expect(program.commands[0].options[1].short).toEqual('-p');
+      expect(program.commands[0].options[1].long).toEqual('--terraformPlan');
+      expect(program.commands[0].options.length).toEqual(2);
+      expect(program.commands[0]._description).toEqual('test');
     });
-    it('Should return json object', () => {
-      let result = show('spec/lib/fixtures/simple-create.plan');
-      expect (typeof result).toEqual('string');
+    it('program show command with arguments', () => {
+      program.parse(['show', '-p', 'ble']);
+      expect(program.commands[1].options[0].required).not.toBeFalsy();
+      expect(program.commands[1].options[0].flags).toEqual('-p, --terraformPlan <terraformPlan>');
+      expect(program.commands[1].options[0].short).toEqual('-p');
+      expect(program.commands[1].options[0].long).toEqual('--terraformPlan');
+      expect(program.commands[1].options.length).toEqual(1);
+      expect(program.commands[1]._description).toEqual('show');
+
     });
+    it('program gettfjson command with arguments', () => {
+      program.parse(['gettfjson']);
+      expect(program.commands[2].options.length).toEqual(0);
+      expect(program.commands[2]._argsDescription).toEqual('Download tfjson');
+      expect(program.commands[2]._description).toEqual('gettfjson');
+    });
+
+    it('program gettfjson command with arguments', () => {
+      program.parse(['gettfjson']);
+      expect(program.commands[2].options.length).toEqual(0);
+      expect(program.commands[2]._argsDescription).toEqual('Download tfjson');
+      expect(program.commands[2]._description).toEqual('gettfjson');
+    });
+
   });
 });
