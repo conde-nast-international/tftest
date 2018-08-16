c6cf056aadc7baa42e1feac4f4f906f06251f67a
Renato Castro <renato@yld.io>
2018-08-16 12:25:27 +0100

    add raw args test

--- spec/bin/tftest.spec.js
+++ spec/bin/tftest.spec.js
@@ -3,7 +3,8 @@ const { show, cli, program } = require('../../bin/tftest');
 describe('tftest', () => {
   describe('program', () => {
     it('program test command with arguments', () => {
-      program.parse(['test', '-t', 'bla', '-p', 'ble']);
+      const args = ['test', '-t', 'bla', '-p', 'ble'];
+      program.parse(args);
       expect(program.commands[0].options[0].required).not.toBeFalsy();
       expect(program.commands[0].options[0].flags).toEqual('-t, --terraformFolder <terraformFolder>');
       expect(program.commands[0].options[0].short).toEqual('-t');
@@ -14,22 +15,27 @@ describe('tftest', () => {
       expect(program.commands[0].options[1].long).toEqual('--terraformPlan');
       expect(program.commands[0].options.length).toEqual(2);
       expect(program.commands[0]._description).toEqual('test');
+      expect(program.rawArgs).toEqual(args);
     });
     it('program show command with arguments', () => {
-      program.parse(['show', '-p', 'ble']);
+      const args = ['show', '-p', 'ble'];
+      program.parse(args);
       expect(program.commands[1].options[0].required).not.toBeFalsy();
       expect(program.commands[1].options[0].flags).toEqual('-p, --terraformPlan <terraformPlan>');
       expect(program.commands[1].options[0].short).toEqual('-p');
       expect(program.commands[1].options[0].long).toEqual('--terraformPlan');
       expect(program.commands[1].options.length).toEqual(1);
       expect(program.commands[1]._description).toEqual('show');
+      expect(program.rawArgs).toEqual(args);
 
     });
     it('program gettfjson command with arguments', () => {
-      program.parse(['gettfjson']);
+      const args = ['gettfjson'];
+      program.parse(args);
       expect(program.commands[2].options.length).toEqual(0);
       expect(program.commands[2]._argsDescription).toEqual('Download tfjson');
       expect(program.commands[2]._description).toEqual('gettfjson');
+      expect(program.rawArgs).toEqual(args);
     });
   });
 });
