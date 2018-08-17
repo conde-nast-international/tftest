9846db25ddb873e3e01bfe913e7b9dc072c0b4ca
Renato Castro <renatoc@users.noreply.github.com>
2018-08-17 15:07:24 +0100

    Fix loading of Jasmine config (#31)
    * Fix loading of Jasmine config
    
    * Change jasmine var to kebab case
    
    * Add readFileSafeJsonParse method, basically a wrapper on two null returning functions.
    
    * Update lib/runner to use generic.js as module properties, this allows spy'ing on the methods. Also stop runner printing, it should just return false.
    
    * Ensure cli makes use of return from runner, prints error and exits with non-zero or success and zero exit code.
    
    * Update specs to use libGeneric and spy on read method to allow error to be thrown if the config for the runner isn't present

--- spec/lib/runner.spec.js
+++ spec/lib/runner.spec.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const { getRunnerContext, getRunner } = require('../../lib/generic.js');
+const libGeneric = require('../../lib/generic.js');
+const { getRunnerContext, getRunner } = libGeneric;
 const Plan = require('../../lib/plan.js');
+const Runner = require('../../lib/runner.js');
 const fixtureFile = (fileName) => path.join(__dirname, 'fixtures', fileName);
 const simpleTestFolder = fixtureFile('vpc_example');
 const simpleTestPlanBin = path.resolve(simpleTestFolder, 'plan.bin');
 const simpleTestPlanJsonFilename = path.resolve(simpleTestFolder, 'plan.bin.json');
 
-// expect in a global context is somehow being overwritten when embedding...
+// expect/spyOn in a global context is somehow being overwritten when embedding...
 const specExpect = expect;
+const specSpyOn = spyOn;
 
-const getJsonFile = (fn) => JSON.parse(fs.readFileSync(fn));
-
-const getSimplePlan = () => getJsonFile(simpleTestPlanJsonFilename);
+const getSimplePlan = () => libGeneric.readFileSafeJsonParse(simpleTestPlanJsonFilename);
 
 describe('the test runner', () => {
   let runner = null;
@@ -23,15 +24,28 @@ describe('the test runner', () => {
   afterEach(() => {
     context = null;
   });
+
   it('should parse a plan to internal property', () => {
     specExpect(runner.plan.plan).toEqual(getSimplePlan());
   });
 
-  it('should setup jasmine correctly', function () {
+  it('should throw an error when jasmineConfigRunner isn\'t present in package.json', (done) => {
+    specSpyOn(libGeneric, 'readFileSafeJsonParse').and.returnValue({ 'invalid': 'package.json' });
+    try {
+      runner = new Runner(simpleTestFolder, simpleTestPlanBin);
+      specExpect('jasmineConfigRunner missing didn\'t fail like it should do...').toEqual(true);
+      done();
+    } catch(e) {
+      specExpect(e).toEqual(new Error(`Jasmine config is not present.`));
+      done();
+    }
+  });
+
+  it('should setup jasmine correctly', () => {
     specExpect(runner.plan.plan).toEqual(getSimplePlan());
   });
 
-  it('should execute tests correctly', function (done) {
+  it('should execute tests correctly', (done) => {
     // specExpect(runner.plan.plan).toEqual(getSimplePlan());
     runner.execute((success) => {
       specExpect(success).toEqual(true);
@@ -44,7 +58,7 @@ describe('the test runner', () => {
     const changeWindowFolder = fixtureFile('ec2_example_with_changeWindow');
     const changeWindowFolderPlanBin = path.resolve(changeWindowFolder, 'plan.bin');
     const changeWindowFolderPlanJsonFilename = path.resolve(changeWindowFolder, 'plan.bin.json');
-    const changeWindowPlan = getJsonFile(changeWindowFolderPlanJsonFilename);
+    const changeWindowPlan = libGeneric.readFileSafeJsonParse(changeWindowFolderPlanJsonFilename);
     const changeWindowTests = require(path.resolve(changeWindowFolder, 'tests.js'));
     let runWrong = getRunner(changeWindowFolder, changeWindowFolderPlanBin);
     specExpect(runWrong.plan.plan).toEqual(changeWindowPlan);
@@ -61,7 +75,7 @@ describe('the test runner', () => {
     const wrongTestsPlanBin = path.resolve(wrongTestsFolder, 'plan.bin');
     const wrongTestsPlanJsonFilename = path.resolve(wrongTestsFolder, 'plan.bin.json');
     let runWrong = getRunner(wrongTestsFolder, wrongTestsPlanBin);
-    specExpect(runWrong.plan.plan).toEqual(getJsonFile(wrongTestsPlanJsonFilename));
+    specExpect(runWrong.plan.plan).toEqual(libGeneric.readFileSafeJsonParse(wrongTestsPlanJsonFilename));
     runWrong.execute((success) => {
       specExpect(success).toEqual(false);
       done();
